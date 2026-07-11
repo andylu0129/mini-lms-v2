37c3fa261a211a2bce8c237843917ed917933fac
andylu0129 <andylu19980101@gmail.com>
2026-07-12 00:00:50 +1000

    implemented sign out mechanism with app header component

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -1,5 +1,7 @@
+import { ROUTES } from '@/constants/routes';
 import { createServerClient } from '@supabase/ssr';
 import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
 
 /**
  * Especially important if using Fluid compute: Don't put this client in a
@@ -34,4 +36,34 @@ export async function clearAuthCookies() {
       cookieStore.delete(cookie.name);
     }
   });
+}
+
+export async function getVerifiedUserData() {
+  const supabase = await createClient();
+  const {
+    data: { user },
+    error,
+  } = await supabase.auth.getUser();
+
+  if (!user?.id || error) {
+    redirect(ROUTES.SIGN_IN);
+  }
+
+  return { firstName: user.user_metadata?.first_name, lastName: user.user_metadata?.last_name };
+}
+
+export async function getUserDataFromToken() {
+  const supabase = await createClient();
+  const { data, error } = await supabase.auth.getClaims();
+
+  if (error || !data?.claims?.email) {
+    redirect(ROUTES.SIGN_IN);
+  }
+
+  return {
+    firstName: data.claims?.user_metadata?.first_name,
+    lastName: data.claims?.user_metadata?.last_name,
+    email: data.claims?.email,
+    role: data.claims?.user_role,
+  };
 }
